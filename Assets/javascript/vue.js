dc94acbfcd31acc3858d4e8c333aefeef09aca36
Pedro Vega <pedrovega468@yahoo.com>
2022-11-15 20:56:05 -0300

    quedaStats

--- Assets/javascript/vue.js
+++ Assets/javascript/vue.js
@@ -5,11 +5,18 @@ const app = createApp({
     data(){
         return {
             events: [],
+            eventsComing: [],
+            eventsPast: [],
             eventsFilter: [],
+            eventsFilterComing: [],
+            eventsFilterPast: [],
             categories: [],
             checked: [],
             checkedSelect: [],
-            searchI: ''
+            searchI: '',
+            id: new URLSearchParams(location.search).get("id"),
+            eventId:[]
+
         }
     },
     created(){
@@ -18,6 +25,12 @@ const app = createApp({
             .then(data => {
                 this.events = data.events
                 this.eventsFilter = data.events
+                this.dateNow = data.currentDate
+                this.eventsComing = this.events.filter(event => this.dateNow < event.date)
+                this.eventsPast = this.events.filter(event => this.dateNow > event.date)
+                this.eventsFilterComing = this.eventsComing
+                this.eventsFilterPast = this.eventsPast
+                this.eventId = this.events.find(item => item._id == this.id)
                 this.getCategory()
             })
             .catch(err => console.log(err))
@@ -33,12 +46,25 @@ const app = createApp({
     },
     computed: {
         filter(){
+            //HOME
             const filterChecked = this.events.filter(event => this.checked.includes(event.category) || this.checked.length === 0)
             this.eventsFilter = filterChecked.filter(event => event.name.toLowerCase().trim().includes(this.searchI.toLowerCase().trim()))
+            //UPComing
+            const filterCheckComing = this.eventsComing.filter(event => this.checked.includes(event.category) || this.checked.length === 0)
+            this.eventsFilterComing = filterCheckComing.filter(event => event.name.toLowerCase().trim().includes(this.searchI.toLowerCase().trim()))
+            //PAST
+            const filterCheckPast = this.eventsPast.filter(event => this.checked.includes(event.category) || this.checked.length === 0)
+            this.eventsFilterPast = filterCheckPast.filter(event => event.name.toLowerCase().trim().includes(this.searchI.toLowerCase().trim()))
         },
         filterSelect(){
             const filterCheckedSelect = this.events.filter(event => this.checkedSelect.includes(event.category) || this.checkedSelect === "Select Category")
             this.eventsFilter = filterCheckedSelect.filter(event => event.name.toLowerCase().trim().includes(this.searchI.toLowerCase().trim()))
+            //UPcomingSelect
+            const filterCheckedSelectComing = this.eventsComing.filter(event => this.checkedSelect.includes(event.category) || this.checkedSelect === "Select Category")
+            this.eventsFilterComing = filterCheckedSelectComing.filter(event => event.name.toLowerCase().trim().includes(this.searchI.toLowerCase().trim()))
+            //PAST
+            const filterCheckedSelectPast = this.eventsPast.filter(event => this.checkedSelect.includes(event.category) || this.checkedSelect === "Select Category")
+            this.eventsFilterPast = filterCheckedSelectPast.filter(event => event.name.toLowerCase().trim().includes(this.searchI.toLowerCase().trim()))
         }
     }
 })
